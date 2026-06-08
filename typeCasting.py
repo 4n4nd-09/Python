593da08bc9e5c447385da644b7cc50a4cb212bdb
Anand <anandkumar001k@gmail.com>
2026-06-08 16:22:51 +0530

    TypeCasting

---
+++ typeCasting.py
@@ -0,0 +1,16 @@
+name = "steve rogers"
+age = 55
+gpa = 4.5
+is_student = True
+
+print(type(age))
+print(type(name))
+print(type(gpa))
+print(type(is_student))
+
+gpa = int(gpa)
+print(gpa)
+age = float(age)
+print(age)
+age = str(age)
+print(age)
