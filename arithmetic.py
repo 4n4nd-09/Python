a8ce1af2dfa0c1e5d3c3106fad3c2679dc1ee42f
Anand <anandkumar001k@gmail.com>
2026-06-08 15:44:37 +0530

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,24 @@
+#Addition
+friends = 5
+friends += 1
+print(friends)
+#Subtraction
+cake = 5
+cake -= 2
+print(cake)
+#multiplication
+pizza = 5
+pizza *= 3;
+print(pizza)
+#divison
+bread = 6
+bread /= 2
+print(bread)
+
+cheese = 10
+cheese //=3
+print(cheese)
+
+candy = 10
+remainingCandies = candy % 3
+print(remainingCandies)
