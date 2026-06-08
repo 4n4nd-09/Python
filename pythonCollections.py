b5026da2a21841441decbe0fd7112525cb9d2a5d
Anand <anandkumar001k@gmail.com>
2026-06-08 17:17:17 +0530

    Collections

---
+++ pythonCollections.py
@@ -0,0 +1,22 @@
+#List[] = mutable, most flexible
+#Tuple() = immutable, faster
+#Set{} = mytable, unordered, No duplicates
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits[0] = "mango"
+# fruits.append("pineapple")
+# fruits.remove("banana")
+# fruits.pop(0)
+# fruits.clear()
+# for fruit in fruits:
+#     print(fruit,end= " ")
+
+# fruits = ("apple", "banana", "cherry")
+# for fruit in fruits:
+#     print(fruit, end=" ")
+
+
+fruits = {"apple", "banana", "cherry"}
+for fruit in fruits:
+    print(fruit)
+
